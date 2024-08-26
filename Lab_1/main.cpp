54a0e33a390812ef9c0a2fca623966b4192f0f04
Santixn35 <orley.gonzalez@udea.edu.co>
2024-08-26 12:52:21 -0500

    Mi primer commit

---
+++ Lab_1/main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{   /*
+    //ejercicio 1
+    int a,b;
+    float c;
+    cout <<"Ingrese el numero A\n";
+    cin>>a;
+
+    cout <<"Ingrese el numero B\n";
+
+    cin>>b;
+
+    c = a%b;
+    cout <<"El residuo de la division es "<< c;
+
+    return 0;
+    */
+
+    /*
+    //ejercicio 2
+    int a;
+
+    cout<<"Ingrese un numero: ";
+    cin>>a;
+
+    if (a%2 == 0){
+        cout<<a<<" es par";
+    }
+    else{
+        cout<<a<<" Es impar";
+    }
+
+    return 0;
+    */
+
+    //ejercicio 3
+    float a,b;
+
+    cout<<"Ingrese 2 numeros:\n";
+    cout<<"a: ";
+    cin>>a;
+    cout<<"b: ";
+    cin>>b;
+
+    if (a>b){
+        cout<<a;
+    }
+    else{
+        cout<<b;
+    }
+    return 0;
+
+    /*
+
+    //ejercicio 4
+    float a,b;
+
+    cout<<"Ingrese 2 numeros:\n";
+    cout<<"a: ";
+    cin>>a;
+    cout<<"b: ";
+    cin>>b;
+
+    if (a>b){
+        cout<<b;
+    }
+    else{
+        cout<<a;
+    }
+    return 0;
+    */
+}
